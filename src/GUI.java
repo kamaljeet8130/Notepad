ac03c11d5e065d3d82329b26da5e3c7cae1f6e42
kamaljeet <kamaljeet8130@gmail.com>
2024-10-07 23:38:23 +0530

    added MenuBar to the Screen

--- src/GUI.java
+++ src/GUI.java
@@ -3,29 +3,64 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class GUI {
-     JFrame window;
-     JTextArea textArea;
+    JFrame window;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem itemNew, itemOpen,itemSave,itemSaveAs,itemExit;
 
     public static void main(String[] args) {
         new GUI();
 
     }
-    public GUI(){
+
+    public GUI() {
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
         window.setVisible(true);
 
 
     }
-    public void createWindow(){
+
+    public void createWindow() {
         window = new JFrame("NotePad");
-        window.setSize(800,600);
+        window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public void createTextArea(){
+
+    public void createTextArea() {
         textArea = new JTextArea();
-        window.add(textArea);
+        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        window.add(scrollPane);
+
     }
 
+    public void createMenuBar() {
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+    }
 
+    public void createFileMenu(){
+        itemNew = new JMenuItem("New");
+        menuFile.add(itemNew);
+        itemOpen = new JMenuItem("Open");
+        menuFile.add(itemOpen);
+        itemSave = new JMenuItem("Save");
+        menuFile.add(itemSave);
+        itemSaveAs = new JMenuItem("Save As");
+        menuFile.add(itemSaveAs);
+
+    }
 }
